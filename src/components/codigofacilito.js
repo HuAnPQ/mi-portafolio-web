29ddfb5406c79782a7eec360796de4a4b5bedc96
Hugo Ponce <hugoponcequiroz@gmail.com>
2020-03-28 14:50:56 -0500

    Mostrar cursos codigo facilito

--- src/components/codigofacilito.js
+++ src/components/codigofacilito.js
@@ -10,32 +10,55 @@ export default () => {
                 score
                 code
               }
+              courses {
+                title
+                progress
+                url
+              }
             }
           }
     }`);
 
     //console.log('data :', data);
     return (
-        <section>
-            <div className="mt-8">
-                <div className="max-w-4xl mx-auto">
-                    <h2 className="text-3xl font-bold text-center">
-                        Mis certificados online en CódigoFacilito
+        <section className="my-12">
+            <div className="max-w-4xl mx-auto">
+                <h2 className="text-3xl font-bold text-center">
+                    Mis certificados online en CódigoFacilito
                     </h2>
-                    <div className="flex mt-6">
-                        {
-                            data.codigofacilitoJson.data.certificates.map(certificado => (
-                                <div className="shadow p-4 bg-white mr-4">
-                                    <h4 className="font-bold">{certificado.title}</h4>
-                                    <div className="text-center">
-                                        <span className="inline-block bg-green-200 text-green-700 p-2 radius">
-                                            Calificacíon: {certificado.score}
-                                        </span>
-                                    </div>
+                <div className="flex mt-6">
+                    {
+                        data.codigofacilitoJson.data.certificates.map(certificado => (
+                            <div className="shadow p-4 bg-white mr-4 rounded">
+                                <h4 className="font-bold">{certificado.title}</h4>
+                                <div className="text-center">
+                                    <span className="inline-block bg-green-200 text-green-700 p-2 radius">
+                                        Calificacíon: {certificado.score}
+                                    </span>
                                 </div>
-                            ))
-                        }
-                    </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+
+            <div className="max-w-4xl mx-auto">
+                <h2 className="text-3xl font-bold text-center">
+                    Mis cursos
+                    </h2>
+                <div className="flex mt-6">
+                    {
+                        data.codigofacilitoJson.data.courses.map(course => (
+                            <div className="shadow p-4 bg-white mr-4 rounded">
+                                <h4 className="font-bold">{course.title}</h4>
+                                <div className="text-center">
+                                    <span className="inline-block bg-green-200 text-green-700 p-2 radius">
+                                        Proceso: {course.progress}
+                                    </span>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </section>
