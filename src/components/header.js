559ec2adef00f881720b36cd879ce90e70d2bd58
Hugo Ponce <hugoponcequiroz@gmail.com>
2020-03-28 09:08:47 -0500

    Add ilustracion en encabezado

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import ilustracion from '../imgs/testing-and-qa-outsourcing.svg';
 
 export default () => (
-    <header className="bg-green-500">
+    <header className="bg-gray-300">
         <div className="container mx-auto p-12 max-w-4xl flex justify-center items-center">
             <div className="flex-1">
                 <h1>Hola! Soy Hugo</h1>
                 <p>QA Testing Tecnico - TTA(Produbanco)</p>
             </div>
-            <img></img>
+            <img src={ilustracion} alt="Imagen de QA Testing"></img>
         </div>
     </header>
 );
