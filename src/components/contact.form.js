2992a2e30b112899d1ef136eb1557df05ea31cca
Hugo Ponce <hugoponcequiroz@gmail.com>
2020-03-28 09:35:59 -0500

    Formulario de contacto

---
+++ src/components/contact.form.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default () => (
+    <form className="mt-16 text-center">
+        <label
+            for="contact-content"
+            className="block text-green-700 text-sm font-bold mb-2">
+            Cuéntame de tu proyecto, el cual quieres mejorar:
+        </label>
+        <div className="flex shadow rounded bg-white border p-2">
+            <textarea
+                id="contact-content"
+                name="contact-content"
+                className="flex-1 py-2 px-3 text-green-700 focus:outline-none focus:shadow-outline"
+            ></textarea>
+            <button>Enviar</button>
+        </div>
+    </form>
+);
--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ilustracion from '../imgs/testing-and-qa-outsourcing.svg';
+import Form from './contact.form';
 
 export default () => (
     <header className="bg-gray-300">
-        <div className="container mx-auto p-12 max-w-4xl flex justify-center items-center">
-            <div className="flex-1">
-                <h1>Hola! Soy Hugo</h1>
-                <p>QA Testing Tecnico - TTA(Produbanco)</p>
+        <div className="container mx-auto p-12 max-w-4xl">
+            <div className="flex justify-center items-center">
+                <div className="flex-1">
+                    <h1 className="font-bold text-green-700 text-6xl">
+                        Hola! Soy Hugo
+                    </h1>
+                    <p className="text-xl font-light">QA Testing Técnico - TTA (Produbanco)</p>
+                </div>
+                <img src={ilustracion} alt="Imagen de QA Testing"
+                    style={{ height: "300px", width: "500px" }}
+                ></img>
             </div>
-            <img src={ilustracion} alt="Imagen de QA Testing"></img>
+            <div>
+                <Form />
+            </div>
         </div>
+
     </header>
 );
