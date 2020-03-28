7e38ba2c37cc11c8d9d2678495a42730983fa3a5
Hugo Ponce <hugoponcequiroz@gmail.com>
2020-03-28 00:13:01 -0500

    Encabezado - 1

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => (
+    <header className="bg-green-500">
+        <div className="container mx-auto p-12 max-w-4xl flex justify-center items-center">
+            <div className="flex-1">
+                <h1>Hola! Soy Hugo</h1>
+                <p>QA Testing Tecnico - TTA(Produbanco)</p>
+            </div>
+            <img></img>
+        </div>
+    </header>
+);
