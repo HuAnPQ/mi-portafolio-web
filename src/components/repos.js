e05de93a0256b7319f408d119676713b182e6df3
Hugo Ponce <hugoponcequiroz@gmail.com>
2020-03-28 12:58:27 -0500

    Mostrar conteo total de respositorios

--- src/components/repos.js
+++ src/components/repos.js
@@ -5,6 +5,7 @@ import Repo from '../components/repo';
 export default () => {
 
     const [Repos, setRepos] = useState([]);
+    const [reposCount, setReposCount] = useState([]);
 
     useEffect(() => {
         const key = "repos";
@@ -12,6 +13,9 @@ export default () => {
 
         if (myRepos) {
             myRepos = JSON.parse(myRepos);
+            
+            setReposCount(myRepos.length);
+
             myRepos = myRepos.slice(1, 13);
             return setRepos(myRepos);
         }
@@ -22,6 +26,8 @@ export default () => {
             myRepos = await fetch(endpoint).then(r => r.json());
             sessionStorage.setItem(key, JSON.stringify(myRepos));
 
+            setReposCount(myRepos.length);
+
             myRepos = myRepos.slice(1, 13);
             setRepos(myRepos);
         }
@@ -30,7 +36,7 @@ export default () => {
 
 
     return (
-        <div className="max-w-4xl mx-auto">
+        <div className="max-w-4xl mx-auto my-12">
             <header className="text-center">
                 <h2 className="text-3xl font-bold">Mi trabajo en Open Source</h2>
                 <p>Colaboración y contribucion de código</p>
@@ -42,6 +48,13 @@ export default () => {
                     })
                 }
             </ul>
+            <div className="mt-8 text-center">
+                <a href="https://github.com/HuAnPQ"
+                    className="btn" target="_blank" rel="noopener noreferrer"        
+                >
+                    Ver más en GitHub ({reposCount})
+                </a>
+            </div>
         </div>
     )
 };
